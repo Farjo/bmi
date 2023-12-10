f1e7c1ffac7c506cc3afc4b5743cc86db23b050f
Farjo <mail@frazeroades.com>
2023-12-10 14:39:07 +0100

    Remove roundImperial

--- calculate-conversions.js
+++ calculate-conversions.js
@@ -1,10 +1,6 @@
 export default calculateConversions
 import { yardsToMetres, metresToYards, poundsToKg, kgToPounds } from './modules/conversions.js'
 import poundsToStones from './modules/pounds-to-stones.js'
-import roundImperial from './modules/round-imperial.js'
-
-const roundStonesPounds = roundImperial('stone', 'pound', 14)
-const roundFeetInches = roundImperial('feet', 'inches', 12)
 
 /**
  * 
@@ -15,8 +11,8 @@ const roundFeetInches = roundImperial('feet', 'inches', 12)
 function calculateConversions(changedField, fields) {
   const { kg, stone, pound, metres, feet, inches } = fields
   if (changedField === 'kg') {
-    const lbs = kgToPounds(kg)
-    return roundStonesPounds(poundsToStones(lbs), 0)
+    const lbs = Math.round(kgToPounds(kg))
+    return poundsToStones(lbs)
   }
   if (changedField === 'stone' || changedField === 'pound') {
     const kg = poundsToKg(stone * 14 + pound).toFixed(1)
@@ -25,8 +21,8 @@ function calculateConversions(changedField, fields) {
   if (changedField === 'metres') {
     const feetDecimal = metresToYards(metres) * 3
     const feet = Math.floor(feetDecimal)
-    const inches = (feetDecimal - feet) * 12
-    return roundFeetInches({ feet, inches })
+    const inches = Math.floor((feetDecimal - feet) * 12)
+    return { feet, inches }
   }
   if (['feet', 'inches'].includes(changedField)) {
     const yards = (feet / 3) + (inches / 36)
